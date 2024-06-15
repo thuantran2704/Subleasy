624ef50801e58f0aabf5d9b0ec621d937f393c69
thuantran2704 <thuantran2704@gmail.com>
2024-06-14 20:44:03 -0400

    add link

--- bookingApp/src/Header.jsx
+++ bookingApp/src/Header.jsx
@@ -5,12 +5,13 @@ export default function Header(){
     const {user} = useContext(UserContext);
     return(
         <header className= "p-4 flex justify-between">
-        <a href="" className = "flex items-center gap-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+        <a href="https://www.example.com" className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-        </svg>
-        <span className = "font-bold text-xl">airbnb</span>
+          </svg>
+          <span className="font-bold text-xl">airbnb</span>
         </a>
+
         <div className='flex border gap-2 border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-332'>
           <div>Anywhere</div>
           <div className='border-l border-gray-300 font-bold'></div>
