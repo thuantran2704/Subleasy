51ec9b8adf4601a2d03ae850e6462bcd87efc17a
thuantran2704 <thuantran2704@gmail.com>
2024-06-16 22:27:41 -0400

    added some additional fillable informations and features

---
+++ api/models/model.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+    // What does a long term apartment listing have?
+    
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        address: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        zip: {
+            type: String,
+            required: true
+        }
+    },
+    bedrooms: {
+        type: Number,
+        required: true
+    },
+    bathrooms: {
+        type: Number,
+        required: true
+    },
+    amenities: {
+        hasPool: {
+            type: Boolean,
+            default: false
+        },
+        hasGym: {
+            type: Boolean,
+            default: false
+        },
+        hasParking: {
+            type: Boolean,
+            default: false
+        },
+        isPetFriendly: {
+            type: Boolean,
+            default: false
+        },
+        hasInternet: {
+            type: Boolean,
+            default: false
+        },
+        hasAirConditioning: {
+            type: Boolean,
+            default: false
+        },
+        hasHeating: {
+            type: Boolean,
+            default: false
+        },
+        hasLaundry: {
+            type: Boolean,
+            default: false
+        },
+    },
+    availabilityStart: {
+        type: Date,
+        required: true
+    },
+    availabilityEnd: {
+        type: Date,
+        required: true
+    },
+    isFurnished: {
+        type: Boolean,
+        required: true
+    }
+})
