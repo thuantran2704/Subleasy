25ac88000f26c1768bcf0f9b6f8e05e22e7302f9
thuantran2704 <thuantran2704@gmail.com>
2024-06-19 22:41:31 -0400

    no changes, just added a comment and commented out some stuff about the server

---
+++ api/dbConnect.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    const connectionParams = {useNewUrlParser: true};
+    mongoose.connect(process.env.MONGO_URL, connectionParams);
+    
+    mongoose.connection.on('connected', () => {
+        console.log('connected to db');
+    });
+    mongoose.connection.on("error", (err) => {
+        console.log(`Mongoose default connection has occured ${err} error`);
+    });
+    
+    mongoose.connection.on("disconnected", () => {
+        console.log("Mongoose default connection is disconnected");
+    });
+}
+
+
+module.exports = dbConnect;
