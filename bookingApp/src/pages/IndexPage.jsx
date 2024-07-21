a185b84183d7230ed043435a48862e4aff43e03a
thuantran2704 <thuantran2704@gmail.com>
2024-07-20 22:00:15 -0400

    added a button for adding listing

--- bookingApp/src/pages/IndexPage.jsx
+++ bookingApp/src/pages/IndexPage.jsx
@@ -1,7 +1,48 @@
-export default function IndexPage(){
-    return(
+
+import{Link} from "react-router-dom";
+import {useState} from 'react';
+import axios from "axios";
+
+export default function IndexPage() {
+    // JavaScript functions to open and close the form
+    const openForm = () => {
+        document.getElementById('myForm').style.display = 'block';
+    }
+
+    const closeForm = () => {
+        document.getElementById('myForm').style.display = 'none';
+    }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    async function handleAddListing(ev){
+        ev.preventDefault();
+        try{
+        await axios.post('/login',{email,password});
+        alert('Login succesful');
+        } catch(e) {
+            alert('Login failed');
+        }
+    }
+
+    return (
         <div>
-            index page 
+            {/* A button to open the popup form */}
+            <button className="open-button" onClick={openForm}>+</button>
+
+            {/* The form */}
+            <div className="form-popup" id="myForm" style={{ display: 'none' }}>
+            <form className="max-w-md mx-auto" onSubmit={handleAddListing}>
+                <input type="email" 
+                        placeholder="your@email.com" 
+                        value = {email} 
+                        onChange={ev => setEmail(ev.target.value)}/>
+                <input type = "password" 
+                        placeholder="password"
+                        value = {password} 
+                        onChange = {ev => setPassword(ev.target.value)}/>
+                <button className = "primary">Submit</button>
+            </form>
+            </div>
         </div>
-        );
+    );
 }
