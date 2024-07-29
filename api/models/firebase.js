a00030cd36e4b3e0a948100b33d218d6c4bf6d68
thuantran2704 <thuantran2704@gmail.com>
2024-07-28 22:21:46 -0400

    add packages for firebase

---
+++ api/models/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {
+    GooogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signOut,
+} from "firebase/auth";
+
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+} from "firebase/firestore";
